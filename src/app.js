277d4c60b2420a4a9c00a7935d55392986e18d3d
codeTukai <tukaighosh753@gmail.com>
2026-06-08 13:02:36 +0530

    feat: implement Express server setup and add initial route; update MongoDB connection logging

--- src/app.js
+++ src/app.js
@@ -1,4 +1,16 @@
 import express from 'express'
 
 const app = express()
+const PORT=3000
 
+app.get('/',function(req,res){
+console.log("Server running");
+
+})
+
+app.listen(PORT, ()=>{
+    console.log(`PORT RUNNING ON ${PORT}`);
+    
+})
+
+export {app}
--- src/db/index.js
+++ src/db/index.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 import { DB_NAME } from '../constants.js'
 
 const connectedDB = async () => {
-    console.log("uri",process.env.MONGODB_URI);
+    // console.log("uri",process.env.MONGODB_URI);
     
     try {
         const databaseInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(databaseInstance);
+        // console.log(databaseInstance);
         
 
         console.log(`\n Database successfully Connected !! DB HOST: ${databaseInstance.connection.host}`);
