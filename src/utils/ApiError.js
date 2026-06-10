d8c6b5a5714dfc6aec54941825ba67aa2702290d
codeTukai <tukaighosh753@gmail.com>
2026-06-10 19:44:42 +0530

    fix: correct super constructor call in ApiError class

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -5,7 +5,7 @@ class ApiError extends Error{
         errors = [],
         stack
     ){
-        super(message)
+        super(message)  //ReferenceError: Must call super constructor in derived class before accessing 'this'
         this.statusCode = statusCode,
         this.data = data,
         this.message = message,
