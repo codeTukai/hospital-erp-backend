3d4b1aa41c579dfc6fddc7fe923c8748ac35f232
codeTukai <tukaighosh753@gmail.com>
2026-06-12 07:33:25 +0530

    complete doctor models

--- src/models/doctor.models.js
+++ src/models/doctor.models.js
@@ -4,7 +4,29 @@ const doctorSchema = new Schema({
     doctorId: {
         type: Schema.Types.ObjectId,
         ref:"User"
+    },
+    specialization:{
+        type: String
+    },
+    experienced:{
+        type: String
+    },
+    qualification:{
+        type: String
+    },
+    experience:{
+        type: Number
+    },
+    consultationFees:{
+        type: Number
+    },
+    availableDate:{
+        type:[String]
+    },
+    availableTime:{
+        type: String
     }
+
 },{timestamps})
 
 export const Doctor = mongoose.model("Doctor", doctorSchema)
