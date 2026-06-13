3f89b1b49e14e921e5c70ad68b95c14caafa0709
codeTukai <tukaighosh753@gmail.com>
2026-06-13 14:20:18 +0530

    feat: add cloudinary upload utility and configuration; remove unused middleware file

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+
+
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadCloudinary = async (localFilPath) => {
+    try {
+        if(!localFilPath) return null;
+
+        const response = await cloudinary.uploader.upload(localFilPath,
+            {
+                resource_type : "auto"
+            }
+        )
+
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilPath)
+
+        return null
+    }
+}
+
+export {uploadCloudinary}
