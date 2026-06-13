6995047cf1effeee61190cd43a82b749fb4e3283
codeTukai <tukaighosh753@gmail.com>
2026-06-13 13:51:13 +0530

    feat: add cloudinary and multer dependencies; fix token generation method

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -66,14 +66,15 @@ userSchema.methods.isPasswordCompared = async function(password){
 userSchema.methods.generateAccessToken = function(){
      return jwt.sign(
         {
-            _id = this._id,
+            _id : this._id,
             email: this.email,
             username: this.username,
             fullName: this.fullName
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
-            expireIn:process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY,
+            
         }
      )
 }
