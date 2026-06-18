95986133352400cee0b9252bd687978faa24532c
codeTukai <tukaighosh753@gmail.com>
2026-06-18 08:10:14 +0530

    feat: update user model to include mobile number; enhance user registration comments

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,16 +3,30 @@ import {ApiError} from '../utils/ApiError.js';
 import { asyncHandler} from '../utils/asyncHandler.js'
 
 
-const userRegister = asyncHandler(async( req, res )=>{
+const userRegister = asyncHandler(async( req, res ) => {
 
-// const {email, password, userName} = res.body;
+//todos
+//get user details from frontend / Postman
+//check the validation---
+//check existence -- if user already log in or not(username / email)
+//upload image , upload avatar 
+//if images coming from 
+//created object id 
+//entry to db 
+// check for user creation 
+//return response
 
-// console.log("email",email);
+const {email, password, userName, fullName} = res.body; // data coming from db via form and json -- url -- req.param
+
+// console.log("email", email);
 
 
-res.status(200).json({
-    message: "ok"
+
+
+// res.status(200).json({
+//     message: "ok"
+// })
 })
-})
+
 
 export {userRegister}
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -69,7 +69,8 @@ userSchema.methods.generateAccessToken = function(){
             _id : this._id,
             email: this.email,
             username: this.username,
-            fullName: this.fullName
+            fullName: this.fullName,
+            mobile: this.mobile
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
